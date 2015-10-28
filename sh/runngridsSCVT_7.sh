8b20dd15eaf93b2a7939b4cf48e3aa22063dde78
Pedro da Silva Peixoto <pd300@emps-sandbach.ex.ac.uk>
2015-10-28 13:29:55 +0000

    sh

--- sh/runngridsSCVT_7.sh
+++ sh/runngridsSCVT_7.sh
@@ -2,17 +2,51 @@
 
 source inc_vars.sh 
 
-awk '{ if ( NR == 5 ) { print "icos";} else {print $0;} }'  par/mesh.par > par/mesh2.par
-awk '{ if ( NR == 9 ) { print "scvt";} else {print $0;} }'  par/mesh2.par > par/mesh.par
+# Script for convergence analysis on HR95 grids
+#------------------------------------------
 
+echo "            Basic Parameters     "
+echo "---------------------------------"
+echo " "
+cat par/mesh.par
+
+awk '{ if ( NR == 5 ) { print "read";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+awk '{ if ( NR == 9 ) { print "nopt";} else {print $0;} }'  par/mesh2.par > par/mesh.par
 #cp par/mesh2.par par/mesh.par
 
-./imodel par/mesh.par 12 # > log.txt
-./imodel par/mesh.par 42 # > log.txt
-./imodel par/mesh.par 162 # > log.txt
-./imodel par/mesh.par 642 # > log.txt
-./imodel par/mesh.par 2562 # > log.txt
-./imodel par/mesh.par 10242 # > log.txt
-./imodel par/mesh.par 40962 # > log.txt
-./imodel par/mesh.par 160000 # > log.txt
-#./imodel par/mesh.par 600000 # > log.txt
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_1.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_2.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_3.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_4.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_5.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_6.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_7.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+cp par/mesh2.par par/mesh.par
+./imodel
+
+#awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_8.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+#cp par/mesh2.par par/mesh.par
+#./imodel
+
+#awk '{ if ( NR == 17 ) { print "icos_pol_scvt_h1_9.xyz";} else {print $0;} }'  par/mesh.par > par/mesh2.par
+#cp par/mesh2.par par/mesh.par
+#./imodel
