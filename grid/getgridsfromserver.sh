913a9b8a446c35c1a2efcc27f051d416dd1a0456
Pedro Peixoto <pedrosp@ime.usp.br>
2016-03-01 16:29:17 -0300

    ignore

---
+++ grid/getgridsfromserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get grids stored in CFD01
+#rsync -avu pedrosp@cfd01.ime.usp.br:/var/tmp/pedrosp/Work/Programas/MPAS/MPAS-PXT/grids/ --progress .
+
+#Get grids from web server
+wget --recursive --no-parent --no-clobber --convert-links --cut-dirs=3 --no-host-directories http://www.ime.usp.br/~pedrosp/grids/swm/
+rm index.* robots.txt
+find . -type f -name 'index.html*' -delete
